7ecf5ade4b92e7b1c93b5ba3b320c990084cbc23
Helene Martin <helenemartin13@hotmail.com>
2013-06-26 20:11:12 +0100

    Add role to user to support Cancan

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,8 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
   has_many :ideas
+
+  def admin?
+  	role == "admin"
+  end
 end
