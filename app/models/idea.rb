2c50d264323662a0ba4b3095463a79b09efabec3
Helene Martin <helenemartin13@hotmail.com>
2013-07-03 20:16:55 +0100

    Change box size, add medium radio buttons

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,7 +1,7 @@
 class Idea < ActiveRecord::Base
 
 
-  attr_accessible :description, :name, :picture
+  attr_accessible :description, :name, :picture, :medium
   mount_uploader :picture, PictureUploader
   belongs_to  :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130626184544) do
+ActiveRecord::Schema.define(:version => 20130703185646) do
 
   create_table "ideas", :force => true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20130626184544) do
     t.string   "picture"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.string   "medium"
   end
 
   create_table "users", :force => true do |t|
